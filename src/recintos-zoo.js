2754fb5ad5a115b11795bf17a961a7300fc4756b
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-09-06 13:53:39 -0300

    feat: arquivos enviados

---
+++ src/recintos-zoo.js
@@ -0,0 +1,46 @@
+class RecintosZoo {
+
+    analisaRecintos() {
+            const recintos = [
+                { numero: 1, bioma: "savana", tamanhoTotal: 10, animaisExistentes: [{ animal: "Macaco", quantidade: 3 }] },
+                { numero: 2, bioma: "floresta", tamanhoTotal: 5, animaisExistentes: [] },
+                { numero: 3, bioma: "savana" && "rio", tamanhoTotal: 7, animaisExistentes: [{ animal: "Gazela", quantidade: 1 }] },
+                { numero: 4, bioma: "rio", tamanhoTotal: 8, animaisExistentes: [] },
+                { numero: 5, bioma: "savana", tamanhoTotal: 9, animaisExistentes: [{ animal: "Leão", quantidade: 1 }] }
+            ]
+            const animal = {
+                LEAO: { tamanho: 3, bioma: ["savana"], carnivoro: true },
+                LEOPARDO: { tamanho: 2, bioma: ["savana"], carnivoro: true },
+                CROCODILO: { tamanho: 3, bioma: ["rio"], carnivoro: true },
+                MACACO: { tamanho: 1, bioma: ["savana", "floresta"], carnivoro: false },
+                GAZELA: { tamanho: 2, bioma: ["savana"], carnivoro: false },
+                HIPOPOTAMO: { tamanho: 4, bioma: ["savana", "rio"], carnivoro: false }
+            }
+
+            function isAnimalValido(tipo) {
+                return animal.hasOwnProperty(tipo)
+            }
+
+            function isQuantidadeValida(quantidade) {
+                return Number.isInteger(quantidade) && quantidade > 0
+            }
+
+            function isInvalidoAnimal(tipo, quantidade) {
+                if (!isAnimalValido(tipo)) {
+                    return { erro: "Animal inválido" }
+                }
+
+                if (!isQuantidadeValida(quantidade)) {
+                    return { erro: "Quantidade inválida" }
+                }
+            }
+
+            const animalInfo = this.animais[tipo]
+            const tamanhoNecessario = animalInfo.tamanho * quantidade
+            
+        }
+
+
+    }
+
+export { RecintosZoo as RecintosZoo };
